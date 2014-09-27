fba0ad1a010f80cfff0190e3b1599b2cfd114237
nyaocat <n@nyaocat.jp>
2014-09-27 20:17:12 +0900

    gtkmm-3.0を用いて書き直し

--- Makefile
+++ Makefile
@@ -8,10 +8,10 @@ LEXEROBJS=$(wildcard ../scintilla/gtk/Lex*.o)
 all: jabberwock
 
 .cpp.o:
-	g++ `pkg-config --cflags gtk+-3.0` $(INCLUDEDIRS) $(CXXFLAGS) -c $< -o $@
+	g++ -std=c++11 `pkg-config --cflags gtkmm-3.0` $(INCLUDEDIRS) $(CXXFLAGS) -c $< -o $@
 
 jabberwock: jabberwock.o $(LEXEROBJS) ../scintilla/bin/scintilla.a
-	g++ -DGTK $^ -o $@ -lstdc++ `pkg-config --libs gtk+-3.0 gthread-2.0 gmodule-2.0`
+	g++ -DGTK $^ -o $@ -lstdc++ `pkg-config --libs gtkmm-3.0 gthread-2.0 gmodule-2.0`
 
 clean:
 	rm -rf jabberwock *.o
--- jabberwock.cpp
+++ jabberwock.cpp
@@ -1,105 +1,76 @@
-#include <gtk/gtk.h>
-
+#include <gtkmm.h>
 #include <Scintilla.h>
 #include <SciLexer.h>
 #define PLAT_GTK 1
 #include <ScintillaWidget.h>
 
-void open(GtkAction *, gpointer )
-{
-  g_print("未実装");
-}
+Glib::RefPtr<Gtk::UIManager> create_menu() {
+  Glib::ustring ui_info = "<ui>"
+                          "  <menubar name='Menubar'>"
+                          "    <menu name='File' action='File'>"
+                          "      <menuitem name='Open' action='Open'/>"
+                          "      <separator/>"
+                          "      <menuitem name='Quit' action='Quit'/>"
+                          "    </menu>"
+                          "  </menubar>"
+                          "</ui>";
 
-void quit(GtkAction *action, gpointer user_data)
-{
-  GObject *window = G_OBJECT(user_data);
-  g_object_unref(g_object_get_data(window, "ui"));
-  gtk_main_quit();
-}
+  auto actions = Gtk::ActionGroup::create("menu");
 
-const gchar *menu_info =
-  "<ui>"
-  "  <menubar name='Menubar'>"
-  "    <menu name='File' action='File'>"
-  "      <menuitem name='Open' action='Open'/>"
-  "      <separator/>"
-  "      <menuitem name='Quit' action='Quit'/>"
-  "    </menu>"
-  "  </menubar>"
-  "</ui>";
+  actions->add(Gtk::Action::create("File", "_File"));
+  actions->add(Gtk::Action::create("Open", Gtk::Stock::OPEN));
+  actions->add(Gtk::Action::create("Quit", Gtk::Stock::QUIT),
+               sigc::ptr_fun(Gtk::Main::quit));
 
-GtkActionEntry entries[] = {
-  {"File", NULL, "_File"},
-  {"Open", GTK_STOCK_OPEN, "_Open", "<control>O", "Open file", G_CALLBACK(open)},
-  {"Quit", GTK_STOCK_QUIT, "_Quit", "<control>Q", "Quit this program", G_CALLBACK(quit)},
-};
+  auto ui = Gtk::UIManager::create();
 
-GtkUIManager* create_menu(GtkWidget *parent)
-{
-  GtkUIManager *ui;
-  GtkActionGroup *actions;
-
-  ui = gtk_ui_manager_new();
-  actions = gtk_action_group_new("menu");
-  gtk_action_group_add_actions(actions, entries, sizeof(entries) / sizeof(entries[0]), parent);
-  gtk_ui_manager_insert_action_group(ui, actions, 0);
-  gtk_ui_manager_add_ui_from_string(ui, menu_info, -1, NULL);
-  gtk_window_add_accel_group(GTK_WINDOW(parent), gtk_ui_manager_get_accel_group(ui));
+  ui->insert_action_group(actions);
+  ui->add_ui_from_string(ui_info);
   return ui;
 }
 
-int exit_app(GtkWidget*, GdkEventAny*, gpointer) {
-   gtk_main_quit();
-   return 1;
+int main(int argc, char *argv[]) {
+
+  Gtk::Main kit(argc, argv);
+
+  Gtk::Window window;
+  window.set_default_size(600, 400);
+
+  Gtk::Box box(Gtk::ORIENTATION_VERTICAL, 2);
+  window.add(box);
+
+  box.pack_start(*create_menu()->get_widget("/Menubar"), Gtk::PACK_SHRINK);
+  {
+    GtkWidget *editor;
+    ScintillaObject *sci;
+
+    editor = scintilla_new();
+    sci = SCINTILLA(editor);
+
+    scintilla_set_id(sci, 0);
+    scintilla_send_message(sci, SCI_STYLECLEARALL, 0, 0);
+    scintilla_send_message(sci, SCI_SETLEXER, SCLEX_CPP, 0);
+    scintilla_send_message(sci, SCI_SETKEYWORDS, 0,
+                           reinterpret_cast<sptr_t>("int char"));
+    scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_COMMENT, 0x008000);
+    scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_COMMENTLINE, 0x008000);
+    scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_NUMBER, 0x808000);
+    scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_WORD, 0x800000);
+    scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_STRING, 0x800080);
+    scintilla_send_message(sci, SCI_STYLESETBOLD, SCE_C_OPERATOR, 1);
+    scintilla_send_message(sci, SCI_SETCODEPAGE, SC_CP_UTF8, 0);
+    scintilla_send_message(
+        sci, SCI_INSERTTEXT, 0,
+        reinterpret_cast<sptr_t>("#include <iostream>\n"
+                                 "\n"
+                                 "int main() {\n"
+                                 "    using namespace std;\n"
+                                 "    cout << \"nyaocat\" << endl;\n"
+                                 "}"));
+
+    box.pack_start(*Glib::wrap(editor), true, true, 0);
+  }
+
+  window.show_all();
+  Gtk::Main::run(window);
 }
-
-int main(int argc, char **argv) {
-   GtkWidget *app;
-   GtkWidget *editor;
-   ScintillaObject *sci;
-
-   gtk_init(&argc, &argv);
-   app = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-   editor = scintilla_new();
-   sci = SCINTILLA(editor);
-
-   g_signal_connect(G_OBJECT(app), "delete_event", G_CALLBACK(exit_app), 0);
-   gtk_widget_set_size_request(app, 600, 400);
-
-   scintilla_set_id(sci, 0);
-
-   scintilla_send_message(sci, SCI_STYLECLEARALL, 0, 0);
-   scintilla_send_message(sci, SCI_SETLEXER, SCLEX_CPP, 0);
-   scintilla_send_message(sci, SCI_SETKEYWORDS, 0, reinterpret_cast<sptr_t>("int char"));
-   scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_COMMENT, 0x008000);
-   scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_COMMENTLINE, 0x008000);
-   scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_NUMBER, 0x808000);
-   scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_WORD, 0x800000);
-   scintilla_send_message(sci, SCI_STYLESETFORE, SCE_C_STRING, 0x800080);
-   scintilla_send_message(sci, SCI_STYLESETBOLD, SCE_C_OPERATOR, 1);
-   scintilla_send_message(sci, SCI_SETCODEPAGE, SC_CP_UTF8, 0);
-   scintilla_send_message(sci, SCI_INSERTTEXT, 0,
-     reinterpret_cast<sptr_t>(
-       "#include <iostream>\n"
-       "\n"
-       "int main() {\n"
-       "    using namespace std;\n"
-       "    cout << \"nyaocat\" << endl;\n"
-       "}"));
-
-   GtkWidget* vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
-   gtk_container_add(GTK_CONTAINER(app), vbox);
-   GtkUIManager *ui;
-   GtkWidget *menubar;
-
-   ui = create_menu(app);
-   g_object_set_data(G_OBJECT(app), "ui", (gpointer)ui);
-   menubar = gtk_ui_manager_get_widget(ui, "/Menubar");
-   gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
-   gtk_box_pack_start(GTK_BOX(vbox), editor, TRUE, TRUE, 0);
-
-   gtk_widget_show_all(app);
-   gtk_widget_grab_focus(GTK_WIDGET(editor));
-   gtk_main();
-}
-
